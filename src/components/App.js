0cd31d8e47c89d2c5b992550df01039d6ba179ad
Seyi Agboola <learnseyi@gmail.com>
2021-07-11 21:15:11 -0500

    initial push

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <button className="test-btn"> test-me</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
